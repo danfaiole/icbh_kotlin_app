e8c93a135f8bf2c746226268149b12e2323b7d9b
Daniel Faiole <danfaiole@gmail.com>
2019-05-20 23:55:46 +0000

    added a recycler view for videos, still gotta implement the adapter

--- app/src/main/java/fms/android/discipulosembh/adapters/VideosAdapter.kt
+++ app/src/main/java/fms/android/discipulosembh/adapters/VideosAdapter.kt
@@ -1,4 +1,23 @@
 package fms.android.discipulosembh.adapters
 
-class VideosAdapter {
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+
+class VideosAdapter: RecyclerView.Adapter<VideosAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
 }
