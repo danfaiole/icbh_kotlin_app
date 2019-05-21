beb3253645b55000981b7a2d8efa4ec330b8c69c
Daniel Faiole <danfaiole@gmail.com>
2019-05-21 00:11:33 +0000

    implemented a little more of the videos adapter

--- app/src/main/java/fms/android/discipulosembh/adapters/VideosAdapter.kt
+++ app/src/main/java/fms/android/discipulosembh/adapters/VideosAdapter.kt
@@ -1,23 +1,36 @@
 package fms.android.discipulosembh.adapters
 
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import fms.android.discipulosembh.R
 
 class VideosAdapter: RecyclerView.Adapter<VideosAdapter.ViewHolder>() {
-    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onCreateViewHolder(parent: ViewGroup, p1: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.recycler_view_videos, parent, false)
+
+        return ViewHolder(view)
     }
 
-    override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bindItem("Teste") // Gotta create a model for videos
     }
 
     override fun getItemCount(): Int {
+        // Gotta create a model for videos
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val headerImage = itemView.findViewById<ImageView>(R.id.rv_videos_image)
+        val titleText = itemView.findViewById<TextView>(R.id.rv_videos_title)
+        val descriptionText = itemView.findViewById<TextView>(R.id.rv_videos_description)
 
+        fun bindItem(item: String) {
+            // Gotta create a model for videos
+        }
     }
 }
